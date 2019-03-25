968bf8ba1e6dadb0a9228e14ce4d46d78e7ec21d
Ben <bonwenu@emich.edu>
2019-03-24 23:59:02 -0400

    Added health bar to play and stopped health bars from spinning

--- COSC457FinalProject/Assets/Scripts/PlayerCombat.cs
+++ COSC457FinalProject/Assets/Scripts/PlayerCombat.cs
@@ -11,12 +11,17 @@ public class PlayerCombat : MonoBehaviour
     public LayerMask whatIsEnemy;
     public float attackRange;
     public float damage;
+    public float health;            // Set health to (Desired health/100)-0.1
+    public float tempHealth;
+
+    private Animator anime;
+    [SerializeField] public HealthBar healthBar;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tempHealth = health;
     }
 
     // Update is called once per frame
@@ -40,11 +45,44 @@ public class PlayerCombat : MonoBehaviour
         {
             timeBtwAttack -= Time.deltaTime;
         }
-     }
+        if (health <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (health > .01f)
+        {
+
+
+            if (health < .3f)
+            {
+                healthBar.SetColor(Color.white);
+
+
+                healthBar.SetColor(Color.red);
+
+            }
+        }
+    }
     void OnDrawGizmosSelected() // Methods to sow attack range
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
-        
- }
+
+    public void TakeDamage(float damage)
+    {
+        healthBar.SetSize(health);
+        health -= damage;
+
+        if (health <= 0f)
+        {
+            health = 0f; // Just too make sure its 0
+        }
+        healthBar.SetSize(health / tempHealth);
+        Debug.Log("damage Taken !");
+
+    }
+
+}
