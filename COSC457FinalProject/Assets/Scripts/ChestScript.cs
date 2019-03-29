dbc616bf0fc26ac7fb9f7e039cdb0a2716b9367b
Ben Sussman <bsussma1@emich.edu>
2019-03-29 10:09:36 -0400

    Added Chest
    Added chest object. As a test, chest will open on game startup.

---
+++ COSC457FinalProject/Assets/Scripts/ChestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public Sprite openChest;
+    public float interactRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        this.GetComponent<SpriteRenderer>().sprite = openChest;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
