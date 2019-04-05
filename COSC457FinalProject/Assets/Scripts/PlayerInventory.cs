d5239ebbd37df974c0eec41b31a9e47d683e630a
Ben Sussman <bsussma1@emich.edu>
2019-04-05 12:35:14 -0400

    More Items
    Added health items and weapons

--- COSC457FinalProject/Assets/Scripts/PlayerInventory.cs
+++ COSC457FinalProject/Assets/Scripts/PlayerInventory.cs
@@ -7,19 +7,25 @@ public class PlayerInventory : MonoBehaviour
 {
     [Header("Set Dynamically")]
     public string[] inventory; // THIS IS THE PLAYER INVENTORY
-    public string[] possibleItems; // THIS IS ALL THE ITEMS IN THE GAME
+    public string[] essentialItems; // THIS IS ALL THE CAR ITEMS IN THE GAME
+    public string[] weaponItems; // THIS IS ALL WEAPONS IN THE GAME
+    public string[] healthItems; // THIS IS ALL HEALTH ITEMS IN THE GAME
 
     // Start is called before the first frame update
     void Start()
     {
-        // Can be changed later if there are more items
-        inventory = new string[4];
+        // Can add items here
+        essentialItems = new string[] { "Gas", "Tire", "Keys", "Battery" };
+        weaponItems = new string[] {"Knife", "Gun"};
+        healthItems = new string[] {"Bandage"};
+
+        // Player inventory initialized to empty strings
+        int maxHealthItems = 5;
+        inventory = new string[essentialItems.Length+weaponItems.Length+maxHealthItems];
         for (int i = 0; i < inventory.Length; i++)
         {
             inventory[i] = "";
         }
-
-        possibleItems = new string[] { "Gas", "Tire", "Keys", "Starter" };
     }
 
     // IsInInventory checks to see if the item is in the players inventory already
@@ -42,6 +48,7 @@ public class PlayerInventory : MonoBehaviour
         {
             if (inventory[i].CompareTo("") == 0)
             {
+
                 inventory[i] = item;
                 return;
             }
@@ -51,14 +58,31 @@ public class PlayerInventory : MonoBehaviour
     public void GivePlayerRandomItem()
     {
         System.Random r = new System.Random();
-        int i = 0;
+        int i = 0, j = 0;
+        string[][] possibleItems = new string[3][];
+        possibleItems[0] = essentialItems;
+        possibleItems[1] = weaponItems;
+        possibleItems[2] = healthItems;
         while (true)
         {
-            i = r.Next(0, possibleItems.Length);
-            if (!IsInInventory(possibleItems[i]))
+            j = r.Next(0, possibleItems.Length);
+            i = r.Next(0, possibleItems[j].Length);
+            if (!IsInInventory(possibleItems[j][i]) || (j == 2))
             {
-                AddToInventory(possibleItems[i]);
-                Debug.Log("Added " + possibleItems[i] + " to inventory");
+                int m = 0;
+                if (j == 2)
+                {
+                    for (int k = 0; k < inventory.Length; k++)
+                    {
+                        if (possibleItems[j][i].CompareTo(inventory[k]) == 0)
+                            m++;
+                        if (m >= 5)
+                            GivePlayerRandomItem(); // This is bad programming don't do this
+                    }
+                }
+
+                AddToInventory(possibleItems[j][i]);
+                Debug.Log("Added " + possibleItems[j][i] + " to inventory");
                 return;
             }
         }
@@ -66,9 +90,9 @@ public class PlayerInventory : MonoBehaviour
 
     public bool HasAllItems()
     {
-        for (int i = 0; i < possibleItems.Length; i++)
+        for (int i = 0; i < essentialItems.Length; i++)
         {
-            if (!IsInInventory(possibleItems[i]))
+            if (!IsInInventory(essentialItems[i]))
             {
                 return false;
             }
