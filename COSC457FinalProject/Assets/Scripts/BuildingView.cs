238cafd89909973a04253ab29e626cfd95f2b2c0
jbement <jbement@emich.edu>
2019-04-13 22:40:50 -0400

    Animation
    I reverted to a previous version because I caused too many problems trying to fix the house code, but i fixed the right direction animation

---
+++ COSC457FinalProject/Assets/Scripts/BuildingView.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingView : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    public Sprite image1;
+    public Sprite image2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Sprite is changed to inside of the house
+            this.GetComponent<SpriteRenderer>().sprite = image2;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Sprite is changed to inside of the house
+            this.GetComponent<SpriteRenderer>().sprite = image1;
+        }
+    }
+}
